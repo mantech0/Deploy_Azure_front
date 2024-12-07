d7a97b173aa313d6f5c496152d7632045c3131e1
mantech0 <man0519tech@gmail.com>
2024-12-08 01:27:02 +0900

    7回目コミ ット

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone'
-};
-
-export default nextConfig;
-
+    reactStrictMode: true,
+    // その他の設定があればここに追加
+  }
+  
+  module.exports = nextConfig
