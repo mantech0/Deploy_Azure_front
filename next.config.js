155e30e2c13042534eaed9e2482ec185a472b0bf
mantech0 <man0519tech@gmail.com>
2024-12-11 02:29:26 +0900

    Fix Next.js configuration

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  experimental: {
-    outputStandalone: true
-  },
   env: {
     NEXT_PUBLIC_API_URL: 'https://tech0-gen-8-step3-testapp-py2-26.azurewebsites.net'
   }
