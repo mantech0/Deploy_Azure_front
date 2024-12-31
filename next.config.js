b102adf73c058303b8a527fda111bc40f6d7217b
mantech0 <man0519tech@gmail.com>
2024-12-31 17:18:32 +0900

    fix: Update Next.js configuration for standalone mode

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  assetPrefix: process.env.NODE_ENV === 'production' 
+    ? 'https://tech0-gen-8-step3-testapp-node2-26.azurewebsites.net' 
+    : '',
+  images: {
+    domains: ['tech0-gen-8-step3-testapp-node2-26.azurewebsites.net']
+  }
 }
 
 module.exports = nextConfig
